3a005ddf9353df92ee9a51b540b1d2ba5d55a70f
Grace Knox <gknox@graces-macbook-air.local>
2016-03-10 10:36:03 -0600

    random image for imageOne

---
+++ Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  Swift Hack Presentation
+//
+//  Created by Grace Knox on 3/10/16.
+//  Copyright © 2016 Grace Knox. All rights reserved.
+//
+
+import UIKit
+
+class Image: NSObject {
+    let images : UIImage
+    
+    init(newImage : UIImage) {
+        images = newImage
+    }
+}
--- Swift Hack Presentation/ViewController.swift
+++ Swift Hack Presentation/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    var images = [Image]()
+    var random = 0
+    
+    @IBOutlet weak var imageOne: UIImageView!
+    @IBOutlet weak var imageTwo: UIImageView!
+    @IBOutlet weak var imageThree: UIImageView!
+    @IBOutlet weak var imageFour: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        images.append(Image(newImage : UIImage(named: "queen.png")!))
+        images.append(Image(newImage : UIImage(named: "king.png")!))
+        images.append(Image(newImage : UIImage(named: "jack.png")!))
+        images.append(Image(newImage : UIImage(named: "ace.png")!))
+        imageOne.image = UIImage(named: "king.png")
+        imageTwo.image = UIImage(named: "queen.png")
+        imageThree.image = UIImage(named: "jack.png")
+        imageFour.image = UIImage(named: "ace.png")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func shuffleButton(sender: AnyObject) {
+        selectImage() 
     }
+    
+    func selectImage() -> Image {
+        random = Int(arc4random_uniform(4))
+       imageOne.image = images[random].images
+       return images[random]
+   }
 
 
 }
